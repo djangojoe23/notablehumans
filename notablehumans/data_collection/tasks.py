6c18ab28b201b9814344dca3cbf3f3349dbd2fcd
Joe Meyer <17205605+djangojoe23@users.noreply.github.com>
2025-01-27 10:54:57 -0500

    Third commit

--- notablehumans/data_collection/tasks.py
+++ notablehumans/data_collection/tasks.py
@@ -4,4 +4,4 @@ from celery import shared_task
 @shared_task()
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
-    return "this is a task to test my commits on github"
+    return "this is a task to test my commits on github one last time"
